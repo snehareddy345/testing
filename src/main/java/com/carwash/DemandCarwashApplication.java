db077d63068e3752677e627e6e16342a1e2714fd
snehareddy345 <70002052+snehareddy345@users.noreply.github.com>
2020-09-03 13:39:07 +0530

    Add files via upload

---
+++ src/main/java/com/carwash/DemandCarwashApplication.java
@@ -0,0 +1,13 @@
+package com.carwash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemandCarwashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemandCarwashApplication.class, args);
+	}
+
+}
